68f4cd5d4ae7dc39ab8861243d870ce583f6e9f3
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-11-10 16:40:00 +0100

    init

---
+++ cmake/TObejct2JSONDependencies.cmake
@@ -0,0 +1,20 @@
+find_package(MySQL REQUIRED)
+find_package(ROOT 6.06.02 REQUIRED)
+find_package(CURL REQUIRED)
+find_package(ZeroMQ REQUIRED)
+
+o2_define_bucket(
+  NAME
+  o2_tobejct2json
+
+  DEPENDENCIES
+  ${ROOT_LIBRARIES}
+  ${CURL_LIBRARIES}
+  ${ZeroMQ_LIBRARY_STATIC}
+
+  SYSTEMINCLUDE_DIRECTORIES
+  ${ROOT_INCLUDE_DIR}
+  ${MYSQL_INCLUDE_DIRS}
+  ${CURL_INCLUDE_DIRS}
+  ZeroMQ_INCLUDE_DIR
+)
