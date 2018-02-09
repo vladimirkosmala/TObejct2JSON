95dcc53d236670c4972bc6f93b1a20f5f0049904
Vladimir Kosmala <vladimirkosmala@users.noreply.github.com>
2018-02-09 16:05:30 +0100

    added debug messages to tests

--- tests/test_zmq.cpp
+++ tests/test_zmq.cpp
@@ -13,21 +13,30 @@
 
 using namespace std;
 using namespace AliceO2::Common;
+using namespace o2::quality_control::core;
 
 int main (void) {
+  int error;
+
   // Open a socket
   void *context = zmq_ctx_new();
   void *socket = zmq_socket(context, ZMQ_REQ);
-  zmq_connect(socket, "tcp://localhost:5555");
+  error = zmq_connect(socket, "tcp://localhost:5555");
+  if (error) {
+    string details;
+    details += "Unable to connect zmq: ";
+    details += zmq_strerror(zmq_errno());
+    BOOST_THROW_EXCEPTION(FatalException() << errinfo_details(details));
+  }
 
   // Send request
   string request = "ping";
   zmq_msg_t message;
   zmq_msg_init(&message);
   zmq_msg_init_data(&message, (void*)request.data(), request.size(), NULL, NULL);
-  std::cout << "Sending request: " << request << std::endl;
-  int result = zmq_msg_send(&message, socket, 0);
-  if (result != 0) {
+  QcInfoLogger::GetInstance() << "Sending request: " << request << infologger::endm;
+  error = zmq_msg_send(&message, socket, 0);
+  if (error) {
     string details;
     details += "Unable to send zmq message: ";
     details += zmq_strerror(zmq_errno());
@@ -35,9 +44,15 @@ int main (void) {
   }
 
   // Answer
-  zmq_msg_recv(&message, socket, 0);
+  error = zmq_msg_recv(&message, socket, 0);
+  if (error) {
+    string details;
+    details += "Unable to connect zmq: ";
+    details += zmq_strerror(zmq_errno());
+    BOOST_THROW_EXCEPTION(FatalException() << errinfo_details(details));
+  }
   string response((const char*)zmq_msg_data(&message), zmq_msg_size(&message));
-  std::cout << "Received answer: " << response << std::endl;
+  QcInfoLogger::GetInstance() << "Received answer: " << response << infologger::endm;
 
   // Close socekt
   zmq_close(socket);
